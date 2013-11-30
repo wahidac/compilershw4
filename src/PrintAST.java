d3f3384dcf4be9917c32d1e99fdc020a5033c893
Wahid <wahidac@gmail.com>
2013-11-29 18:07:07 -0800

    finished mem read

--- src/PrintAST.java
+++ src/PrintAST.java
@@ -7,6 +7,9 @@ import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 import cs132.vapor.ast.*;
 
+//NOTE: at beginning of function, save any callee saved
+//registers that are going to be used
+
 
 //Traverse tree, swapping variable references to the registers they
 //are assigned to, or a position in memory if they've been spilled
@@ -82,9 +85,46 @@ public class PrintAST extends VInstr.VisitorPR<Integer, String, Throwable>  {
 		return spillString;
 	}
 	
+	//Either assign loaded mem value to resultReg or assign the assigned reg 
+	//to this var to the resultReg
+	public String spillOrSubstituteVariable(String resultReg,String variable,Integer indentation) {
+		ArrayList<String> spilledVariablesForFunc = spilledVariables.get(currentFunction);
+		HashMap<String,String> regAssignments = registerAssignments.get(currentFunction);
+		String returnString = "";
+		int index = spilledVariablesForFunc.indexOf(variable);
+		if(index != -1) {
+			//Load val from memory and put in result reg
+			returnString = retrieveValueFromMemory(currentFunction, variable, resultReg, indentation);
+		} else {
+			//swap w/ reg we've given 
+			variable = regAssignments.get(variable);
+			if(variable == null)
+				variable = spillRegisters[2];
+			//Assign register val to result reg
+			returnString = assign(resultReg, variable, indentation);
+		}
+		
+		return returnString;
+	}
+	
+	
+	
+	
 	public static boolean isOperandVariable(VOperand operand) {
 		return (operand instanceof VVarRef.Local);
 	}
+	
+	public static String variableFromMemAddress(VAddr varAddr) {
+		if(varAddr instanceof VAddr.Var<?>) {
+			VAddr.Var v = (VAddr.Var)varAddr;
+			String var = v.var.toString();
+			return var;
+		}
+		return null;
+	}
+	
+	
+	
 	
 	@Override
 	//Assignment statement
@@ -97,20 +137,9 @@ public class PrintAST extends VInstr.VisitorPR<Integer, String, Throwable>  {
 		String assignment = "";
 		
 		if(isOperandVariable(arg1.source)) {
-			//Operand is a variable. 
-			int index = spilledVariablesForFunc.indexOf(val);
-			if(index != -1) {
-				//Load val from memory and put in a spill register
-				assignment = retrieveValueFromMemory(currentFunction, val, spillRegisters[0], indentation);
-				val = spillRegisters[0];
-			} else {
-				//swap w/ reg we've given 
-				val = regAssignments.get(val);
-				if(val == null)
-					val = spillRegisters[2];
-			}
+			assignment = spillOrSubstituteVariable(spillRegisters[0], val, indentation);
+			val = spillRegisters[0];
 		}
-		
 		
 		int index = spilledVariablesForFunc.indexOf(dest);
 		if(index != -1) {
@@ -175,20 +204,11 @@ public class PrintAST extends VInstr.VisitorPR<Integer, String, Throwable>  {
 		for(argReg = 0; argReg < arg1.args.length; argReg++) {
 			VOperand op = arg1.args[argReg];
 			if(isOperandVariable(op)) {
+				String spillReg = spillRegisters[argReg];
 				String val = op.toString();
-				int index = spilledVariablesForFunc.indexOf(val);
-				if(index != -1) {
-					//load from mem
-					String spillReg = spillRegisters[argReg];
-					builtInCall = concatentateInstructions(builtInCall,retrieveValueFromMemory(currentFunction, val, spillReg, indentation));
-					argRegisters[argReg] = spillReg;
-				} else {
-					//swap w/ reg we've given 
-					val = regAssignments.get(val);
-					if(val == null)
-						val = spillRegisters[2];
-					argRegisters[argReg] = val;
-				}
+				String s = spillOrSubstituteVariable(spillReg, val, indentation);
+				builtInCall = concatentateInstructions(builtInCall, s);
+				argRegisters[argReg] = spillReg;
 			}
 		}
 
@@ -228,15 +248,81 @@ public class PrintAST extends VInstr.VisitorPR<Integer, String, Throwable>  {
 	}
 
 	@Override
-	public String visit(Integer arg0, VMemWrite arg1) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+	//Memory write operation
+	public String visit(Integer indentation, VMemWrite arg1) throws Throwable {
+		String memWrite = "";
+		ArrayList<String> spilledVariablesForFunc = spilledVariables.get(currentFunction);
+		HashMap<String,String> regAssignments = registerAssignments.get(currentFunction);
+		String sourceVar = arg1.source.toString();
+		
+		if(isOperandVariable(arg1.source)) {
+			//Operand is a variable. 
+			String spillReg = spillRegisters[0];
+			memWrite = spillOrSubstituteVariable(spillReg, sourceVar, indentation);
+			sourceVar = spillReg;
+		}
+		
+		if(arg1.dest instanceof VMemRef.Global) {
+			VMemRef.Global ref = (VMemRef.Global)arg1.dest;
+			String baseAddr = variableFromMemAddress(ref.base);
+			if(baseAddr != null) {
+				//Base address is a var
+				String resultReg = spillRegisters[1];
+				String s = spillOrSubstituteVariable(resultReg, baseAddr, indentation);
+				String lhs = accessMemory(resultReg, ref.byteOffset);
+				memWrite = concatentateInstructions(memWrite, s, assign(lhs,sourceVar,indentation));
+			} else {
+				String lhs = accessMemory(ref.base.toString(), ref.byteOffset);
+				memWrite = concatentateInstructions(memWrite, assign(lhs,sourceVar,indentation));
+
+			}
+		} else {
+			assert(false);
+		}
+		
+		
+		return memWrite;
 	}
 
 	@Override
-	public String visit(Integer arg0, VMemRead arg1) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+	public String visit(Integer indentation, VMemRead arg1) throws Throwable {
+		String memRead = "";
+		ArrayList<String> spilledVariablesForFunc = spilledVariables.get(currentFunction);
+		HashMap<String,String> regAssignments = registerAssignments.get(currentFunction);
+		String destVar = arg1.dest.toString();
+
+		
+		String sourceResultReg = "";
+		if(arg1.source instanceof VMemRef.Global) {
+			VMemRef.Global ref = (VMemRef.Global)arg1.source;
+			String baseAddr = variableFromMemAddress(ref.base);
+			sourceResultReg = spillRegisters[0];
+			if(baseAddr != null) {
+				//Base address is a var
+				String s = spillOrSubstituteVariable(sourceResultReg, baseAddr, indentation);
+				String rhs = assign(sourceResultReg,accessMemory(sourceResultReg, ref.byteOffset),indentation);
+				memRead = concatentateInstructions(memRead, s,rhs);
+			} else {
+				String rhs = accessMemory(ref.base.toString(), ref.byteOffset);
+				memRead = concatentateInstructions(memRead, assign(sourceResultReg,rhs,indentation));
+			}
+		} else {
+			assert(false);
+		}
+		
+		
+		int index = spilledVariablesForFunc.indexOf(destVar);
+		if(index != -1) {
+			//Store val in memory
+			memRead = concatentateInstructions(memRead, storeValueInMemory(currentFunction, destVar, sourceResultReg, indentation));
+		} else {
+			String destReg = regAssignments.get(destVar);
+			if(destReg == null)
+				destReg = spillRegisters[2];
+			memRead = concatentateInstructions(memRead, assign(destReg, sourceResultReg, indentation));
+		}
+		
+		return memRead;
 	}
 
 	@Override
